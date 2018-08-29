b33f4c4c405a1d83b4165054dfa821414e4fc4ab
关晓丹 <GXD9983@163.com>
2018-08-29 17:54:07 +0800

    运行不起来？

--- client/app.js
+++ client/app.js
@@ -5,3 +5,10 @@ import App from './APP.jsx';
 
 ReactDom.hydrate(<App/>, document.getElementById('root')) // 使用reactDom插件将App组件挂载到root下面，用JSX语法编译后生成的DOM换掉HTML文件中root下面的所有内容，用生成的js文件注入到HTML中。从哪里来的HTML文件？在webpack使用的html-webpack-plugin插件，会在编译时候生成HTML文件。就是dist文件夹下面的index.html
  
+// 当需要热更新的代码出现的时候，把整个App重新加载一遍
+if(module.hot){
+    module.hot.accept('./App.jsx', ()=>{
+        const NextApp = require('./App.jsx').default
+        ReactDom.hydrate(<NextApp/>, document.getElementById('root'))
+    })
+}
