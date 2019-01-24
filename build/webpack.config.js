a528778e57351ce4c97a087a06e33c2630d1bbf9
关晓丹 <GXD9983@163.com>
2019-01-24 15:45:56 +0800

    2-4 webpack 的基础配置

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
 
 module.exports = {
+    mode:"development",
     entry:{
         app: path.join(__dirname,'../client/app.js')
     },
@@ -8,5 +10,23 @@ module.exports = {
         filename:'[name].[hash].js',
         path: path.join(__dirname, '../dist'),
         publicPath: ''
-    }
+    },
+    module:{
+        rules:[
+            {
+                test: /.jsx$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /.js$/,
+                loader: 'babel-loader',
+                exclude:[
+                    path.join(__dirname,'../node_modules')
+                ]
+            },
+        ]
+    },
+    plugins:[
+        new HTMLPlugin() // 可以生成一个HTML页面，同时在编译时将我们的entry注入到页面中，路径根据output里的配置生成
+    ]
 }
--- client/app.js
+++ client/app.js
@@ -1,1 +1,5 @@
-alert('123') 
+import React from 'react';
+import ReactDOM from 'react-dom'; // 将react组件渲染到DOM中
+import App from './App.jsx';
+
+ReactDOM.render(<App />, document.body) // 将组件挂载到页面上
